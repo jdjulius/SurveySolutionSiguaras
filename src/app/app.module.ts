8f23f03db1e54ab2c4929849dde9d543fee7faf1
Jose Cruz <jose.crperez@gmail.com>
2023-03-06 17:43:37 -0600

    init code

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './modules/auth/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    PageNotFoundComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth())
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'survey-4ef4a',
+    appId: '1:839974821115:web:b9b88e26396709b511a45e',
+    storageBucket: 'survey-4ef4a.appspot.com',
+    apiKey: 'AIzaSyBlzA2Mos7N-XSvi-NL8mq50xDS9MHBhmk',
+    authDomain: 'survey-4ef4a.firebaseapp.com',
+    messagingSenderId: '839974821115',
+  },
+  production: false,
+}
